39c71dbedc6525749c446b371956a2876aafb630
Yogesh K Gautam <yogi.2210@gmail.com>
2022-12-24 18:30:01 +0530

    navbar and header section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
+import List from './pages/list/List';
 
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/hotels' element={<List/>} />
+        <Route path='/hotels/:id' element={<Hotel/>} />
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
