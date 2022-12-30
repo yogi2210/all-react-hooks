f951e066c23834faf74f848910a8d9cd14bf55fc
Yogesh K Gautam <yogi.2210@gmail.com>
2022-12-31 03:04:57 +0530

    integrate

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,32 +1,38 @@
 import './featured.css'
 
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
 
 const Featured = () => {
+    const {data, loading, error} = useFetch("http://localhost:8800/api/hotels/countByCity?cities=madrid,london,berlin")
+    console.log(data)
   return (
+    
     <div className='featured'>
+    { loading ? "Loading please wait" : 
+      <>  
         <div className="featuredItem">
             <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/xphoto/800x640/140040902.jpg?k=cc640a6bf0d56605d51d2efd34e35408aa55daab0b586be469fe7f9b6ed76518&o=" alt="" />
             <div className="featuredTitles">
-                <h1>Trondhium</h1>
-                <h2>123 properties</h2>
+                <h1>London</h1>
+                <h2>{data[0]} properties</h2>
             </div>
         </div>
         <div className="featuredItem">
             <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/xphoto/800x640/140052809.jpg?k=32c638c8195324aed3c06e30208500ecc6a4a9095738babee43b48f9b739f3a7&o=" alt="" />
             <div className="featuredTitles">
-                <h1>Sixt</h1>
-                <h2>233 properties</h2>
+                <h1>Madrid</h1>
+                <h2>{data[1]} properties</h2>
             </div>
         </div>
         <div className="featuredItem">
             <img className='featuredImg' src="https://cf.bstatic.com/xdata/images/xphoto/800x640/140040902.jpg?k=cc640a6bf0d56605d51d2efd34e35408aa55daab0b586be469fe7f9b6ed76518&o=" alt="" />
             <div className="featuredTitles">
-                <h1>Dublin</h1>
-                <h2>23 properties</h2>
+                <h1>Berlin</h1>
+                <h2>{data[2]} properties</h2>
             </div>
         </div>
-       
+        </>}
     </div>
   )
 }
